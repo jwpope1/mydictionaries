3cd965bf983edb8904ca81026e1c3739bbe949d0
Jacob Pope <Jacob_Pope4@baylor.edu>
2023-02-06 23:43:04 -0600

    Homework assignment

--- 7_schools_report.py
+++ 7_schools_report.py
@@ -17,3 +17,24 @@ Display report for all universities that have a total price for in-state student
 
 
 """
+import json
+
+infile = open("school_data.json", "r")
+schools = json.load(infile)
+# print(schools)
+print(type(schools))
+
+conference_schools = [372, 108, 107, 130]
+
+# How many school are in this file?
+print(len(schools))
+
+for school in schools:
+    if school["NCAA"]["NAIA conference number football (IC2020)"] in conference_schools:
+        if school["Graduation rate women (DRVGR2020)"] > 75:
+            print(f"University Name: {school('instnm')}")
+            print(
+                f"Graduation rate for women: {school['Graduation rate women (DRVGR2020)']}"
+            )
+            print()
+            print()
